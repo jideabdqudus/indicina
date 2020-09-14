320844f7cbba7b4e352a88a068e17fc8f2f6ccbc
jideabdqudus <jideabdqudus@gmail.com>
2020-09-14 12:05:59 +0100

    Added Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Topbar from "./layout/Topbar";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Topbar/>
     </div>
   );
 };
---
+++ src/layout/Topbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+
+
+const Topbar = () => {
+    return (
+        <div>
+            Nav
+        </div>
+    )
+}
+
+export default Topbar
